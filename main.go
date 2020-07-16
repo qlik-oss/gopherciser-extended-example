22973b9e845f83262dc1f25bea904692c257a735
Daniel Larsson <dlo@qlik.com>
2020-07-16 13:27:27 +0200

    register custom actions

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"github.com/qlik-oss/gopherciser/cmd"
+	
+	// make sure to register custom actions
+	_ "github.com/qlik-oss/gophericser-extended-example/customactions"
+
 )
 
 func main() {
